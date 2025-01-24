82085aaeafba403889b961d6bb793dcf9f5679be
Johnson1123 <onifadejohnson2017@gmail.com>
2025-01-24 10:07:21 -0600

    responsiviness/design

--- src/components/large/Community.jsx
+++ src/components/large/Community.jsx
@@ -3,13 +3,23 @@ import TitleText from "../small/TitleText";
 import ParaText from "../small/ParaText";
 import Donate from "../small/btn/Donate";
 import { DummyImage } from "../../assets/images";
+import { useIsMobile } from "../../utils/isMobile";
 
 function Community() {
+  const isMobile = useIsMobile();
   return (
     <div className="community__container">
-      <div className="community__image-top-wrapper">
-        <img src={DummyImage} alt="" className="big top-left" />
-        <img src={DummyImage} alt="" className="small top-right" />
+      <div className={isMobile ? "flex" : "community__image-top-wrapper"}>
+        <img
+          src={DummyImage}
+          alt=""
+          className={isMobile ? "" : "big top-left"}
+        />
+        <img
+          src={DummyImage}
+          alt=""
+          className={isMobile ? "" : "small top-right"}
+        />
       </div>
       <div className="community__content-wrapper">
         <TitleText
@@ -24,9 +34,17 @@ function Community() {
           <Donate text="Join Our Organization" />
         </div>
       </div>
-      <div className="community__image-buttom-wrapper">
-        <img src={DummyImage} alt="" className="big buttom-left" />
-        <img src={DummyImage} alt="" className="small buttom-right" />
+      <div className={isMobile ? "flex" : "community__image-buttom-wrapper"}>
+        <img
+          src={DummyImage}
+          alt=""
+          className={isMobile ? "" : "small buttom-left"}
+        />
+        <img
+          src={DummyImage}
+          alt=""
+          className={isMobile ? "" : "small buttom-right"}
+        />
       </div>
     </div>
   );
