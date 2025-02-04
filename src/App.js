7ee07d3f2e450cc3f57a4b6cb98ac17f6785bc37
Johnson1123 <onifadejohnson2017@gmail.com>
2025-02-03 22:47:38 -0600

    herosection inverted border correction

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.scss";
 import Footer from "./components/large/Footer";
 import Header from "./components/large/Header";
+import Chat from "./pages/Chat";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
       <Header />
-      <Home />
+      {/* <Home /> */}
+      <Chat />
       <Footer />
     </div>
   );
